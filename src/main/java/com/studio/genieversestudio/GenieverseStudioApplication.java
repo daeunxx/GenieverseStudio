7fa105996694eab6618ee7d9edc743ed81eac872
daeunxx <sss3598@gmail.com>
2023-08-02 16:58:02 +0900

    기본 프로젝트 설정

---
+++ src/main/java/com/studio/genieversestudio/GenieverseStudioApplication.java
@@ -0,0 +1,13 @@
+package com.studio.genieversestudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenieverseStudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenieverseStudioApplication.class, args);
+	}
+
+}
