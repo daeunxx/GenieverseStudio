29ead7f24b2668fdb4c5905f20b7ce1749710d1c
daeunxx <sss3598@gmail.com>
2023-08-08 11:03:00 +0900

    Redis 환경설정 변경

--- src/main/java/com/studio/genieversestudio/GenieverseStudioApplication.java
+++ src/main/java/com/studio/genieversestudio/GenieverseStudioApplication.java
@@ -2,8 +2,9 @@ package com.studio.genieversestudio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class GenieverseStudioApplication {
 
 	public static void main(String[] args) {
